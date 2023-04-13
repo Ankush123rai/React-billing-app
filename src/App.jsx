4a8f29c7418547ace6026112796f7ccf9244509e
Ankush123rai <ankush98rai@gamil.com>
2023-04-13 15:37:20 +0530

    file updated

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BillingForm from './BillingForm/BillingForm'
+
+const App = () => {
+  return (
+    <div>
+      <BillingForm />
+    </div>
+  )
+}
+
+export default App
